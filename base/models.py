f6f4b5d351f5b42343a8c539b8589832355d9189
Sargunjot Singh <sj.singhh@gmail.com>
2021-12-17 16:06:30 -0700

    Performed Code Refactoring

--- base/models.py
+++ base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 ROLES = (
    ('Admin', 'Admin - Can Delete Members'),
@@ -8,13 +7,10 @@ ROLES = (
 )
 
 class Team(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200,null=True, blank=True)
     email = models.EmailField(max_length = 254,null=True, blank=True)
     phone_number = models.CharField(max_length=12,null=True, blank=True)
-    # description = models.TextField(null=True, blank=True)
-    # complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
     role = models.CharField(choices=ROLES, max_length=128, default='Regular')
 
--- base/views.py
+++ base/views.py
@@ -5,8 +5,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Team
 
-# Create your views here.
-
+#Application Views
 
 class TeamList(ListView):
     model = Team
