90ca5c49b5a453caeffd72b98f53126f45e83fcf
Sargunjot Singh <sj.singhh@gmail.com>
2022-01-05 19:17:20 -0700

    Added a validation check for when the input does not match a phone number format.

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 ROLES = (
    ('Admin', 'Admin - Can Delete Members'),
@@ -10,7 +11,8 @@ class Team(models.Model):
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200,null=True, blank=True)
     email = models.EmailField(max_length = 254,null=True, blank=True)
-    phone_number = models.CharField(max_length=12,null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 12 digits are allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=12, null=True,blank=True) # validators should be a list
     create = models.DateTimeField(auto_now_add=True)
     role = models.CharField(choices=ROLES, max_length=128, default='Regular')
 
