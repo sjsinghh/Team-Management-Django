7b8b4b19e03c70374f5efa895a6037253766cc0a
Sargunjot Singh <sj.singhh@gmail.com>
2022-01-05 19:20:48 -0700

    Changed phone number max allowed size to 15

--- base/models.py
+++ base/models.py
@@ -11,8 +11,8 @@ class Team(models.Model):
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200,null=True, blank=True)
     email = models.EmailField(max_length = 254,null=True, blank=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 12 digits are allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=12, null=True,blank=True) # validators should be a list
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits are allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, null=True,blank=True) # validators should be a list
     create = models.DateTimeField(auto_now_add=True)
     role = models.CharField(choices=ROLES, max_length=128, default='Regular')
 
